8c2e1bd5c8a57d101740d4d0c779e617772c1486
jonas1015 <jonasgsmart1015@gmail.com>
2024-08-02 11:43:41 +0300

    Get coders and desplay them on the table

--- src/app/modules/pcva/components/coders/coders.component.ts
+++ src/app/modules/pcva/components/coders/coders.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CodersService } from '../../services/coders/coders.service';
+import { catchError, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-coders',
   templateUrl: './coders.component.html',
   styleUrl: './coders.component.scss'
 })
-export class CodersComponent {
+export class CodersComponent implements OnInit {
+  
+  codersData$?: Observable<any>;
+  loadingData: boolean = false;
+  
+  constructor(private codersService: CodersService){}
+
+  ngOnInit(): void {
+    this.loadingData = true
+    this.codersData$ = this.codersService.getCoders(true).pipe(
+      map((response) => {
+        // this.loadingData = false
+       return response;
+      }),
+      catchError((error: any) => {
+        // this.loadingData = false
+        return error;
+      })
+    );
+  }
 
 }
