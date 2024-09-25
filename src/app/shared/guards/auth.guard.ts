91bef4792f10fcb2bad457f2666e7dbd85621643
jonas1015 <jonasgsmart1015@gmail.com>
2024-09-25 15:27:08 +0300

    Add service to get user roles and privileges

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,7 +1,9 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router } from '@angular/router';
 import { is_authenticated } from '../helpers/auth.helpers';
-import { inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { AuthService } from '../../core/services/authentication/auth.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { lastValueFrom } from 'rxjs';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
@@ -13,3 +15,29 @@ export const authGuard: CanActivateFn = async (route, state) => {
   }
   return authenticated
 };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrivilegeGuard implements CanActivate {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
+    const requiredPrivilege = route.data['requiredPrivilege'];
+    const hasPrivileges = await lastValueFrom(this.authService.hasPrivilege(requiredPrivilege))
+    if (hasPrivileges) {
+      return true;
+    }
+    this.snackBar.open("You don't have permission to access this page", "close", {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 3 * 1000,
+    });
+    return false;
+  }
+}
