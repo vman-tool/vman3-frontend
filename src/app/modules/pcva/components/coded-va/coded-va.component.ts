9b7b2ab5cb6cdf532f07431b4f36d737d24e5f6b
jonas1015 <jonasgsmart1015@gmail.com>
2025-01-09 15:50:39 +0300

    Fixed coded VA table

--- src/app/modules/pcva/components/coded-va/coded-va.component.ts
+++ src/app/modules/pcva/components/coded-va/coded-va.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CodedVaService } from '../../services/coded-va/coded-va.service';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { ViewVaComponent } from 'app/shared/dialogs/view-va/view-va.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-coded-va',
   templateUrl: './coded-va.component.html',
   styleUrl: './coded-va.component.scss'
 })
-export class CodedVaComponent {
+export class CodedVaComponent implements OnInit {
+  current_user: any;
+  codedVas$?: Observable<any>;
+  loadingData: boolean = false;
+  headers?: string[];
+  
+  pageNumber?: number = 0;
+  pageSizeOptions = [10, 20, 50, 100]
+  limit?: number;
 
+  constructor(
+    private codedVaService: CodedVaService,
+    public dialog: MatDialog
+  ) {}
+  
+  ngOnInit() {
+    this.current_user = JSON.parse(localStorage.getItem('current_user') || "")
+    this.loadCodedVAs()
+  }
+
+  loadCodedVAs() {
+      this.loadingData = true
+    this.codedVas$ = this.codedVaService.getCodedVARecords(
+        {
+          paging: true,
+          page_number: this.pageNumber,
+          limit: this.limit,
+        },
+        undefined,
+        this.current_user?.uuid
+      ).pipe(
+        map((response: any) => {
+          if(!this.headers){
+            this.headers = response?.data[0]?.vaId ? Object.keys(response?.data[0])?.filter((column: string) => column.toLowerCase() !== 'id') : []
+          }
+  
+          // TODO: Add total records for pagination to work 
+          
+          this.loadingData = false
+         return response;
+        }),
+        catchError((error: any) => {
+          this.loadingData = false
+          return throwError(() => error);
+        })
+      )
+    }
+
+    onOpenVA(va: any){
+        let dialogConfig = new MatDialogConfig();
+        dialogConfig.autoFocus = true;
+        dialogConfig.width = "95vw";
+        dialogConfig.height = "90vh";
+        dialogConfig.panelClass = "cdk-overlay-pane"
+        dialogConfig.data = {
+          va: va,
+        }
+        this.dialog.open(ViewVaComponent, dialogConfig)
+      }
+  
+    onPageChange(event: any) {
+      this.pageNumber = event.pageIndex > 0 ? event.pageIndex + 1 : event.pageIndex;
+      this.limit = Number(event?.pageSize);
+      this.loadCodedVAs();
+    }
 }
--- src/app/modules/pcva/services/coded-va/coded-va.service.ts
+++ src/app/modules/pcva/services/coded-va/coded-va.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConfigService } from 'app/app.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodedVaService {
 
-  constructor() { }
+  constructor(private configService: ConfigService, private http: HttpClient) { }
+
+  getCodedVARecords(pager?: { paging?: boolean, page_number?: number, limit?: number }, include_deleted?: string, coder?: any) {
+    let params = pager?.paging ? `?paging=${pager?.paging}` : '';
+
+    params = params?.length && pager?.page_number ? params + `&page=${pager?.page_number}` : pager?.page_number ? params + `?page=${pager?.page_number}` : params;
+    params = params?.length && pager?.limit ? params + `&limit=${pager?.limit}` : pager?.limit ? params + `?limit=${pager?.limit}` : params;
+    params = params?.length && include_deleted ? params + `&include_deleted=${include_deleted}` : include_deleted ? params + `?include_deleted=${include_deleted}` : params;
+
+    params = params?.length && coder ? params + `&coder=${coder}` : coder ? params + `?coder=${coder}` : params
+
+
+    return this.http.get(`${this.configService.API_URL}/pcva/get-coded-va${params}`);
+  }
 }
