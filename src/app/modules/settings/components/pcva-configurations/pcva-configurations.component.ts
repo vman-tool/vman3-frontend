1660886ad3602cb8978b531ce389ff053f072eca
jonas1015 <jonasgsmart1015@gmail.com>
2025-02-05 15:12:58 +0300

    Added PCVA configurations in settings

--- src/app/modules/settings/components/pcva-configurations/pcva-configurations.component.ts
+++ src/app/modules/settings/components/pcva-configurations/pcva-configurations.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PCVAConfigurations } from '../../interface';
 import { PcvaSettingsService } from '../../services/pcva-settings.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-pcva-configurations',
@@ -15,18 +16,30 @@ export class PcvaConfigurationsComponent {
     concordanceLevel : 2
   }
 
-  constructor(private pcvaSettingsService: PcvaSettingsService){}
+  constructor(private pcvaSettingsService: PcvaSettingsService, private snackBar: MatSnackBar){}
+
+  notificationMessage(message: string): void {
+    this.snackBar.open(`${message}`, 'close', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3 * 1000,
+    });
+  }
 
   ngOnInit(){
     this.pcvaSettingsService.getPCVAConfigurations().subscribe((response: any) => {
-
-      this.config = response?.data;
+      this.config = response?.data?.length ? response?.data : this.config;
     });
   }
 
   saveConfigurations(){
+    if(this.config.concordanceLevel > this.config.vaAssignmentLimit){
+      this.notificationMessage('Concordance level should not exceed VA assignment limit!');
+      return;
+    }
     this.pcvaSettingsService.savePCVAConfigurations(this.config).subscribe((response: any) => {
-      console.log(response);
+      this.config = response?.data?.length ? response?.data : this.config
+      this.notificationMessage('PCVA Configurations saved successfully!');
     });
   }
 }
