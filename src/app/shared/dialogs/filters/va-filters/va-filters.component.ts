f6f070fcb6901a7fd6f816e84056a968e80cceb7
jonas1015 <jonasgsmart1015@gmail.com>
2025-05-18 21:42:51 +0300

    Initial implementation of the filter by locations

--- src/app/shared/dialogs/filters/va-filters/va-filters.component.ts
+++ src/app/shared/dialogs/filters/va-filters/va-filters.component.ts
@@ -6,6 +6,12 @@ import { CommonModule } from '@angular/common';
 import { DatePipe } from '@angular/common';
 import { SharedModule } from '../../../shared.module';
 import { CustomDropdownComponent } from '../../../components/custom-dropdown/custom-dropdown.component';
+import { UsersService } from 'app/modules/settings/services/users.service';
+import { lastValueFrom, map } from 'rxjs';
+import { FieldLabel, FieldMapping, settingsConfigData, SystemConfig } from 'app/modules/settings/interface';
+import { SettingConfigService } from 'app/modules/settings/services/settings_configs.service';
+import { GenericIndexedDbService } from 'app/shared/services/indexedDB/generic-indexed-db.service';
+import { OBJECTSTORE_VA_QUESTIONS } from 'app/shared/constants/indexedDB.constants';
 @Component({
   selector: 'app-va-filters',
   standalone: true,
@@ -26,18 +32,28 @@ export class VaFiltersComponent implements OnInit {
   endDate?: Date;
   selectedLocation: string[] = [];
   allLocations: any[] = [];
+  current_user?: any
 
   isLoading: boolean = true;
   resetSelection = new EventEmitter<void>();
   closeDropdown = new EventEmitter<void>();
+  
+  systemConfigData?: SystemConfig;
+  fieldMappingData?: FieldMapping;
+  fieldLabels: FieldLabel[] | undefined;
+  selectedLocationType: any;
   constructor(
     private locationService: LocationService,
     private filterService: FilterService,
-    private datePipe: DatePipe // Inject DatePipe
+    private usersService: UsersService,
+    private settingConfigService: SettingConfigService,
+    private genericIndexedDbService: GenericIndexedDbService,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit(): void {
     this.resetFilterData();
+    this.getUserAccessLimit();
     this.locationService.getLocations().subscribe({
       next: (locations) => {
         this.allLocations = locations.map((location: string) => ({
@@ -52,6 +68,20 @@ export class VaFiltersComponent implements OnInit {
       },
     });
   }
+
+  async loadSystemConfigurations(){
+      await lastValueFrom(
+        this.settingConfigService.getSettingsConfig().pipe(map(() => {
+          (data: settingsConfigData | null) => {
+            if (!!data) {
+              this.systemConfigData = data?.system_configs;
+              this.fieldMappingData = data?.field_mapping;
+              this.fieldLabels = data?.field_labels;
+            }
+          }
+        }))
+      )  
+    }
   resetFilterData() {
     // Notify filterService of the reset
     this.filterService.setFilterData({
@@ -61,6 +91,41 @@ export class VaFiltersComponent implements OnInit {
       date_type: undefined,
       ccva_graph_db_source: true,
     } as any);
+  }
+
+  async getUserAccessLimit(){
+    this.loadSystemConfigurations();
+    this.current_user = JSON.parse(localStorage.getItem("current_user") || "{}");
+    const user_roles_data: any = await lastValueFrom(this.usersService.getUserRoles(this.current_user?.uuid));
+    const access_limit = user_roles_data?.data?.access_limit;
+    
+    
+    const savedFieldLabel = this.fieldLabels?.filter((field_label: any) => field_label?.field_id === this.selectedLocationType?.value)[0] || undefined
+          const locationsFromDb = await lastValueFrom(this.settingConfigService.getUniqueValuesOfField(this.selectedLocationType?.value))
+          let locationsFromQuestions: any = await this.genericIndexedDbService.getDataByKeys(OBJECTSTORE_VA_QUESTIONS, [this.selectedLocationType?.value])
+          
+          locationsFromQuestions = locationsFromQuestions?.length ? locationsFromQuestions?.filter((objectedLocation: any) => objectedLocation)[0] : undefined;
+          
+          if(locationsFromQuestions?.value?.options?.length && locationsFromDb?.data?.length){
+            this.allLocations = locationsFromQuestions?.value?.options?.filter((locationToFilter: any) => locationsFromDb?.data?.some((location: any) => locationToFilter?.value === location))?.map((location: any) => {
+              return {
+                name: savedFieldLabel?.options?.hasOwnProperty(location?.value) ? savedFieldLabel?.options[location?.value] : location?.label,
+                value: location?.value,
+                unique: location?.value
+              }
+            })
+          } else {
+            this.allLocations = locationsFromDb?.data?.map((location: any) => {
+              return {
+                name: savedFieldLabel?.options?.hasOwnProperty(location) ? savedFieldLabel?.options[location] : location,
+                value: location,
+                unique: location
+              }
+            }) || []
+          }
+          if(access_limit && this.selectedLocationType?.value === access_limit?.field){
+            this.selectedLocation = this.allLocations?.filter((location) => access_limit?.limit_by?.some((access_limit: any) => location?.value === access_limit?.value))
+          }
   }
 
   applyFilters(): void {
